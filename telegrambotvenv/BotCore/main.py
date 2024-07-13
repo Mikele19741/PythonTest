cf25667ffee300f43048847cac431c8c85004ced
Mikele197411 <mikhailshilkov@gmail.com>
2024-07-13 22:26:51 +0100

    Added Telegram Chat bot on Python

---
+++ telegrambotvenv/BotCore/main.py
@@ -0,0 +1,16 @@
+from telegram import   InlineKeyboardButton, Update, InlineKeyboardMarkup
+from telegram.ext import  ApplicationBuilder, ConversationHandler,CommandHandler, ContextTypes, CallbackQueryHandler
+import bot
+TELEGRAM_BOT_TOKEN='6842381887:AAExB3jeduS7RNVkXX9mSXWAAfSeWLogMjw'
+TELEGRAM_URL='t.me/test_solutions_bot'
+def main():
+   
+    app=ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", bot.start))
+    app.add_handler(CallbackQueryHandler(bot.gender_callback,ContextTypes.DEFAULT_TYPE))
+
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
